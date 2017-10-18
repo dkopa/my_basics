b0c0f18b67bb0bb5d3c9726b1e8606094cecbd93
dkopa <32906402+dkopa@users.noreply.github.com>
2017-10-18 23:50:22 +0200

    To access files in subdirectories ...

---
+++ directory_file_listing.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 23:01:36 2017
+
+@author: dkopa
+Purpose: Access files in subdirectories ...
+"""
+
+import os
+import glob
+
+mydir = '/home/hasnat/Desktop/data/'
+
+dirList = [x for x in os.listdir(mydir) if os.path.isdir(mydir+x)]
+
+numOfDir = len(dirList)
+
+for idc in range(numOfDir):
+    print('========')
+    tCurrDir = mydir+dirList[idc]+'/'
+    print(dirList[idc])
+    print('========')
+    
+    tFileList = [x for x in os.listdir(tCurrDir) if os.path.isfile(tCurrDir+x)]
+    # tFileList = os.listdir(mydir+dirList[idc])
+    numOfFiles = len(tFileList)
+    
+    for fc in range(numOfFiles):
+        print(tCurrDir + tFileList[fc])
+        # DO FURTHER PROCESSING ON EACH FILE
